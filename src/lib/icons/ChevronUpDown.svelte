fd1173d4452ea8305f1991627d1f9af857cb9ba6
hfcRed <hfcred@gmx.net>
2024-10-19 21:19:02 +0200

    Add chevron icon

---
+++ src/lib/icons/ChevronUpDown.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Icon, { type IconNodes, type IconProps } from '$lib/components/Icon.svelte';
+	type $$Props = IconProps;
+
+	const nodes: IconNodes = [
+		['path', { d: 'm7 15 5 5 5-5' }],
+		['path', { d: 'm7 9 5-5 5 5' }]
+	];
+</script>
+
+<Icon {...$$restProps} {nodes} />
