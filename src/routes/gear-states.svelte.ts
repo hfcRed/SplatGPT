c602227064f529ceb2cd70ccc83c392417cacdb8
hfcRed <hfcred@gmx.net>
2024-10-28 16:40:17 +0100

    Update default weapon

--- src/routes/gear-states.svelte.ts
+++ src/routes/gear-states.svelte.ts
@@ -1,4 +1,4 @@
-import type { Weapon } from '$lib/data/weapons';
+import { weapons, type Weapon } from '$lib/data/weapons';
 import { emptyAbility, type Ability } from '$lib/data/abilities';
 
 export type Token = [string, number];
@@ -18,20 +18,5 @@ export const outputPredictions: { [tokens: string]: Token[] } = $state({
 export const isRunning: { [state: string]: boolean } = $state({ state: false });
 
 export const weapon: { [weapon: string]: Weapon } = $state({
-	weapon: {
-		id: 0,
-		name: 'Shooter_Short_00',
-		image: '/src/lib/images/weapons/Path_Wst_Shooter_Short_00.png',
-		referenceKit: 0,
-		sub: {
-			id: 6,
-			name: 'Bomb_Curling',
-			image: '/src/lib/images/subs/6.png'
-		},
-		special: {
-			id: 11,
-			name: 'SpUltraStamp',
-			image: '/src/lib/images/specials/11.png'
-		}
-	}
+	weapon: weapons[0]
 });
