51d8e4810336507ce05183104c6627eb04a5e206
hfcRed <hfcred@gmx.net>
2024-10-31 00:32:11 +0100

    Add icons

---
+++ src/lib/icons/Moon.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Icon, { type IconNodes, type IconProps } from '$lib/components/common/Icon.svelte';
+
+	let { ...rest }: IconProps = $props();
+
+	const nodes: IconNodes = [['path', { d: 'M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z' }]];
+</script>
+
+<Icon {...rest} {nodes} />
